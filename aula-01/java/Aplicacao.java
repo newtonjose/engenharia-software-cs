ff06a4575f26b97e42f352c773184953e99b5746
newtonjose <newtonjose@gsoftiot.com>
2019-04-27 17:46:33 -0300

    adicinando ; e usando leiaute padrão em java

--- aula-01/java/Aplicacao.java
+++ aula-01/java/Aplicacao.java
@@ -2,7 +2,7 @@
 import java.time.LocalDate;
 
 public class Aplicacao {
-    public static void main(String[] args){
-	System.out.println(LocalDate.now().getDayOfWeek())
+    public static void main(String[] args) {
+	System.out.println(LocalDate.now().getDayOfWeek());
     }
 }
