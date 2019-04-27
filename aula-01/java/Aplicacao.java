f47cf5759634eab59ca5096a20e2a81bf96b479a
newtonjose <newtonjose@gsoftiot.com>
2019-04-27 17:48:51 -0300

    usando melhor forma de documentar classe

--- aula-01/java/Aplicacao.java
+++ aula-01/java/Aplicacao.java
@@ -1,6 +1,9 @@
 // Exercicio 04
 import java.time.LocalDate;
 
+/**
+ * Implementação do exercício 04 que retorna dia da semana
+ */
 public class Aplicacao {
     public static void main(String[] args) {
 	System.out.println(LocalDate.now().getDayOfWeek());
