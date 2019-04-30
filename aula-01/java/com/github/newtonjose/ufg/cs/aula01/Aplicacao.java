041f8ee1b95b18c54f40af03cfed08ba4c53a110
kyriosdata <fabio@inf.ufg.br>
2019-04-30 12:21:05 -0300

    melhor documentacao

--- aula-01/java/com/github/newtonjose/ufg/cs/aula01/Aplicacao.java
+++ aula-01/java/com/github/newtonjose/ufg/cs/aula01/Aplicacao.java
@@ -3,7 +3,9 @@ package com.github.newtonjose.ufg.cs.aula01;
 import java.time.LocalDate;
 
 /**
- * Implementação do exercício 04 que retorna dia da semana
+ * Programa que retorna dia da semana do dia corrente.
+ *
+ * <p>Veja detalhes no exercício 4 (aula-01).</p>
  */
 public class Aplicacao {
     public static void main(String[] args) {
