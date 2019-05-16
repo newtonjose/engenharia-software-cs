e46ea95a3a2f93273a26197c7e2749a0c85d51a2
newtonjose <josephnewton001@gmail.com>
2019-05-16 00:47:32 -0300

    test função somaNaturais

---
+++ aula-01/js/test/somaNaturais.test.js
@@ -0,0 +1,22 @@
+const Algoritmos = require("../aula-01");
+const funcao = Algoritmos.somaNaturais;
+
+test("soma dos naturais caso valido", () => {
+    expect(funcao(5)).toBe(12);
+});
+
+test("argumento null/undefined", () => {
+    expect(() => { funcao(); }).toThrow(Error);
+});
+
+test("argumento nao numerico", () => {
+    expect(() => { funcao("abcd"; }).toThrow();
+});
+
+test("valor fora da faixa", () => {
+    expect(() => { funcao(0);}).toThrow(RangeError);
+});
+
+test("valor que não é inteiro", () => {
+    expect(() => { funcao(10.3);}).toThrow(RangeError);
+});
