dda0fc6ac64b8ef9a6396a12cf33bd72483b3971
newtonjose <josephnewton001@gmail.com>
2019-05-28 16:30:54 -0300

    documentação do arquivo do packge

--- aula-01/java/src/main/java/com/github/newtonjose/ufg/cs/aula01/package-info.java
+++ aula-01/java/src/main/java/com/github/newtonjose/ufg/cs/aula01/package-info.java
@@ -1,4 +1,6 @@
 /**
- *
+ * Pacote com a implementação da classes
+ * {@link com.github.newtonjose.ufg.cs.aula01.Algoritmos} e
+ * {@link com.github.newtonjose.ufg.cs.aula01.Aplicacao}.
  */
 package com.github.newtonjose.ufg.cs.aula01;
