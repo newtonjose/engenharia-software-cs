d3e7f3bd0400795343af6b2350b33cd49e334d1f
newtonjose <josephnewton001@gmail.com>
2019-05-28 19:40:07 -0300

    removendo tag <h1>

--- aula-01/java/src/main/java/com/github/newtonjose/ufg/cs/aula01/Algoritmos.java
+++ aula-01/java/src/main/java/com/github/newtonjose/ufg/cs/aula01/Algoritmos.java
@@ -1,16 +1,15 @@
 package com.github.newtonjose.ufg.cs.aula01;
 
 /**
- * <h1>Algoritmos Matemáticos.</h1>
- * Classe que implementa vários algoritmos acerca de propriedades matemáticas
- * e operações com números.
- * <p>Os algoritmos implementados estão disponíveis na
+ * Implementação dos Algoritmos Matemáticos dado na aula 01.
+ *
+ * <p>Classe que implementa vários algoritmos acerca de propriedades matemáticas
+ * e operações com números.Os algoritmos implementados estão disponíveis na
  * <a href="https://drive.google.com/file/d/1zCxtSyjkEvF6T3ieJ0r3_BJii9kx-FMI"/>
  * aula 01.</p>
- * <p></p>
  *
  * @author Josenilton Santos
- * @version 1.0
+ * @version 2.0
  * @since 2019-03-15
  */
 public final class Algoritmos {
