9961c95d1fc00720551d40461d4bb0405cddcbaf
newtonjose <josephnewton001@gmail.com>
2019-06-07 02:03:54 -0300

    classe que verifica tipo de arquivo

--- aula-08/jpeg/java/src/main/java/com/github/newtonjose/cs/aula08/jpeg/main/ProgramaVerificaJPEG.java
+++ aula-08/jpeg/java/src/main/java/com/github/newtonjose/cs/aula08/jpeg/main/ProgramaVerificaJPEG.java
@@ -1,16 +1,21 @@
 package com.github.newtonjose.cs.aula08.jpeg.main;
 
+
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.logging.Logger;
 
-import static java.nio.file.Files.newInputStream;
+import com.github.newtonjose.cs.aula08.jpeg.src.VerificaJpegBytes;
+
 import static java.nio.file.Paths.get;
+
+
 /**
  * Implementação da classe principal do programa JPEG.
- * <p>Classe principal para execução do programa que verifica se um aquivo
- * é do tipo JPEG.
+ * <p>Classe principal para execução da abertura do arquivo e instanciar classe
+ * que verifica se um aquivo é do tipo JPEG.
  *
  * @author Josenilton Santos
  * @version 1.0
@@ -32,8 +37,6 @@ public final class ProgramaVerificaJPEG {
 
     }
 
-
-
     /**
      * Método main que instancia a classe VerificaJPEG e imprime se o arquivo
      * recebido como parâmetro é do tipo JPEG ou não.
@@ -42,13 +45,21 @@ public final class ProgramaVerificaJPEG {
      *             pasta static. //FINDMORE: Necessário documentar exception
      * @throws IOException Caso o aquivo não seja encontrado.
      */
-    public static void main(final String... args) throws IOException {
+    public static void main(final String... args) {
+        try (RandomAccessFile raf = new RandomAccessFile(args[0], "r")) {
 
-        // leitura do arquivo.
-        final Path filePath = get(args[0]);
-        final InputStream ipSt = newInputStream(filePath);
-        final int valor = ipSt.read();
+            final VerificaJpegBytes vjb = new VerificaJpegBytes(raf);
 
-        LOGGER.fine("Valor Inteiro do Arquivo: " + valor);
+            final Path filePath = get(args[0]); //NOPMD: lawofdemeter
+            final int fileLen = (int) Files.size(filePath);
+
+            if (vjb.verificaTipoJpeg(fileLen)) {
+                LOGGER.info("Arquivo é JPEG!");
+            } else {
+                LOGGER.info("Arquivo não JPEG!");
+            }
+        } catch (IOException ioe) {
+            LOGGER.warning(ioe.getMessage());
+        }
     }
 }
---
+++ aula-08/jpeg/java/src/main/java/com/github/newtonjose/cs/aula08/jpeg/src/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Pacote com um única classe que usada pelo ProgramaVerificaJPEG.
+ */
+package com.github.newtonjose.cs.aula08.jpeg.src;
