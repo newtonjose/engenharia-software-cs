0617f55ca9094b34c6500e8472a826f1f73edba3
newtonjose <josephnewton001@gmail.com>
2019-06-10 13:17:26 -0300

    classe para encontrar a linha no arquivo.

---
+++ aula-08/encontre/java/src/main/java/com/github/newtonjose/cs/aula08/encontre/domain/EncontrePalavra.java
@@ -0,0 +1,26 @@
+package com.github.newtonjose.cs.aula08.encontre.domain;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
+/**
+ * Implementação da classe que encontra uma sequência de caracteres
+ * na linha e coluna de um arquivo de texto.
+ *
+ * @author Josenilton Santos
+ * @version 1.0
+ */
+public final class EncontrePalavra {
+
+    private final static Charset UTF8 = Charset.forName("UTF-8");
+
+    private int[] encontraLinhaColuna(final Files file) {
+        // ler todas as linhas e encontra a primeira ocorrencia da palavra
+        return null;
+    }
+
+    private String lerLinha(final int lin, final int col) {
+        // encontra linha apartir da refência.
+        return null;
+    }
+}
