275a6f4a3e3a4e390db12b4c34ed9d9853fa46c1
newtonjose <josephnewton001@gmail.com>
2019-06-18 21:46:53 -0300

    classe para receber dados do arquivo json

--- aula-09/src/main/java/com/github/newtonjose/ufg/cs/domain/notafiscal/NotaFiscal.java
+++ aula-09/src/main/java/com/github/newtonjose/ufg/cs/domain/notafiscal/NotaFiscal.java
@@ -1,9 +1,22 @@
 package com.github.newtonjose.ufg.cs.domain.notafiscal;
 
+import java.util.ArrayList;
+
 /**
  * Created by aluno on 13/06/19.
  */
 public class NotaFiscal {
     private String data;
-    private Double
+    private Double total;
+
+    ArrayList<ItemNotaFiscal> items;
+
+    public NotaFiscal(final String data, final Double total) {
+        this.data = data;
+        this.total = total;
+    }
+
+    public void putItemNotaFiscal(final ItemNotaFiscal item) {
+        this.items.add(item);
+    }
 }
