692bf63808132640245b429659d9d019022bb141
newtonjose <josephnewton001@gmail.com>
2019-07-08 22:47:45 -0300

    converting date to days

--- aula-11/src/main/java/com/github/newtonjose/exemplo/domain/DirencaEntreDatas.java
+++ aula-11/src/main/java/com/github/newtonjose/exemplo/domain/DirencaEntreDatas.java
@@ -7,6 +7,8 @@
 package com.github.newtonjose.exemplo.domain;
 
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Reúne uma data e o dia da semana correspondente.
@@ -28,5 +30,17 @@ public class DirencaEntreDatas {
 
     public String getDiaDaSemana() {
         return diaDaSemana;
+    }
+
+    /**
+     * Calcula a dirence de dias entre duas datas.
+     *
+     * @param dateI Date Data inicial.
+     * @param dateF Date Data final.
+     * @return long Número de dias entre as duas datas.
+     */
+    public static long getDataEmDias(final Date dateI, final Date dateF) {
+        final long diff = dateI.getTime() - dateF.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 }
